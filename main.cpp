fd1087194d3d79b2fb9f462f81cb37e2922dfe5b
ArthoPacini <artho_pacini@icloud.com>
2020-10-14 04:50:12 -0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 
-
 #include "Blockchain.hpp"
-
-
-
-
-
 
 int main()
 {
-
-    Blockchain blockchain(3, 2); //Create blockchain with 3 level difficulty, max 2 transactions per block
-
+    //Create blockchain with difficulty level 3 and 2 transactions per block max
+    Blockchain blockchain(3, 2); 
 
     //add transactions
     //the block is mined when the transaction count reaches maximum value
@@ -23,11 +16,9 @@ int main()
     blockchain.push_transaction({1.3, "Ruth", "Steve"});
     blockchain.push_transaction({1.3, "Joe", "Steve"});
 
-
     //dump the chain
     blockchain.dump();
     
-
     //validates the chain
     std::cout << "Is blockchain valid? " << blockchain.validate() << std::endl;
 }
